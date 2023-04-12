f3c85cd594653726b92a6aa000118ad8d2a6f00f
Chloe Zhang <chloezhang1030@gmail.com>
2023-04-11 17:18:36 -0700

    main menu styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import playerVsPlayerIcon from './assets/player-vs-player.svg';
+import connectFourLogo from './assets/logo.svg';
 import viteLogo from '/vite.svg'
-import './App.scss'
+import './sass/main.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='menu-container main-menu'>
+        <img src={connectFourLogo} alt="connect four logo" id='logo' />
+        <div>
+          <button className="yellowBtn iconBtn" >
+            <div>player vs player</div>
+            <img src={playerVsPlayerIcon} alt="player vs player icon" />
+          </button>
+          <button className='iconBtn'> game rules </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
+
   )
 }
 
