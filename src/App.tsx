c0fa57a44fab6935e221c79c8ae44cdc34b526e8
Chloe Zhang <chloezhang1030@gmail.com>
2023-04-11 18:37:58 -0700

    game state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import reactLogo from './assets/react.svg'
 import playerVsPlayerIcon from './assets/player-vs-player.svg';
 import connectFourLogo from './assets/logo.svg';
-import viteLogo from '/vite.svg'
 import './sass/main.scss'
 
+type GameState = 'IDLE' | 'IN_GAME' | 'PAUSED';
+
+type GameStateActionType = 'START' | 'RESTART' | 'PAUSE' | 'CONTINUE' | 'QUIT';
+
+type GameStateAction = {
+  type: GameStateActionType
+}
+
+function gameStateReducer(gameState: GameState, action: GameStateAction): GameState {
+  const validTransitions: { [key in GameStateActionType]: GameState[] } = {
+    'START': ['IDLE'],
+    'RESTART': ['IN_GAME', 'PAUSED'],
+    'CONTINUE': ['PAUSED'],
+    'PAUSE': ['IN_GAME'],
+    'QUIT': ['PAUSED']
+  };
+
+  if (!validTransitions[action.type].includes(gameState)) {
+    console.error(`invalid action:${action.type} state:${gameState}`);
+    return gameState;
+  }
+
+  switch (action.type) {
+    case 'START': return 'IN_GAME';
+    case 'RESTART': return 'IN_GAME';
+    case 'CONTINUE': return 'IN_GAME';
+    case 'PAUSE': return 'PAUSED';
+    case 'QUIT': return 'IDLE';
+  }
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const initialGameState: GameState = 'IDLE';
+  const [gameState, dispatch] = useReducer(gameStateReducer, initialGameState);
 
   return (
     <div className="App">
+      <div>current state: <b>{gameState}</b></div>
       <div className='menu-container main-menu'>
         <img src={connectFourLogo} alt="connect four logo" id='logo' />
         <div>
-          <button className="yellowBtn iconBtn" >
+          <button className="yellowBtn iconBtn" onClick={() => { dispatch({ type: 'START' }) }}>
             <div>player vs player</div>
             <img src={playerVsPlayerIcon} alt="player vs player icon" />
           </button>
