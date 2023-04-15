964abc25d149a8056bd2beee22b743fea647529a
Chloe Zhang <chloezhang1030@gmail.com>
2023-04-14 23:41:31 -0700

    move marker with pointer

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,33 +2,53 @@ import boardLayerWhiteLarge from "../assets/board-layer-white-large.svg";
 import boardLayerWhiteSmall from "../assets/board-layer-white-small.svg";
 import boardLayerBlackLarge from "../assets/board-layer-black-large.svg";
 import boardLayerBlackSmall from "../assets/board-layer-black-small.svg";
-import React, { FC } from "react";
+import markerRed from "../assets/marker-red.svg";
+import markerYellow from "../assets/marker-yellow.svg";
+import React, { FC, useState } from "react";
 import BoardCell from "./BoardCell";
-import { CellValue, useGameDispatch } from "../shared/GameContext";
+import { CellValue, useGameDispatch, useGameState } from "../shared/GameContext";
 
 type BoardProps = {
     board: CellValue[][],
 }
 
-const VirtualColumn: FC<{ index: number }> = ({index}) => {
+const VirtualColumns = () => {
     const dispatch = useGameDispatch();
+    const {currentPlayer} = useGameState();
+
+    const columns = [0, 1, 2, 3, 4, 5, 6];
+    const [selectedColumn, setSelectedColumn] = useState(0);
 
     function dropCoin() {
-        console.log('clicked')
-        dispatch({type: "DROP", columnIndex: index});
+        dispatch({type: "DROP", columnIndex: selectedColumn});
     }
 
-    return <div className='virtual-column' onClick={dropCoin}/>
+    function selectColumn(index: number) {
+        setSelectedColumn(index);
+    }
+
+    function renderMarker() {
+        if (currentPlayer === 1) return <img className="marker" src={markerRed} alt="player one marker"/>
+        if (currentPlayer === 2) return <img className="marker" src={markerYellow} alt="player two marker"/>
+        return <></>
+    }
+
+    return <div id='virtual-columns'>
+        {columns.map((i) =>
+            (<div key={`column-${i}`} className='virtual-column' onClick={dropCoin} onMouseOver={() => selectColumn(i)}>
+                {(selectedColumn === i) && renderMarker()}
+            </div>)
+        )}
+    </div>
 }
 const Board: FC<BoardProps> = ({board}) => {
+
     return <>
         <picture>
             <source media="(min-width:768px)" srcSet={boardLayerWhiteLarge}/>
             <img src={boardLayerWhiteSmall} alt="board layer white"/>
         </picture>
-        <div id='virtual-columns'>
-            {[0, 1, 2, 3, 4, 5, 6].map((i) => (<VirtualColumn key={`virtual-column-${i}`} index={i}/>))}
-        </div>
+        <VirtualColumns/>
         <div id='virtual-board'>
             {board.map((column, colNum) => (
                 <div key={`column-${colNum}`} className='board-column'>
