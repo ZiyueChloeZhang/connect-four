b6b0e5f03ca14b5ea7f9253ad9e0865e4fac65dc
Chloe Zhang <chloezhang1030@gmail.com>
2023-04-15 00:30:14 -0700

    move marker with keyboard

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,7 +4,7 @@ import boardLayerBlackLarge from "../assets/board-layer-black-large.svg";
 import boardLayerBlackSmall from "../assets/board-layer-black-small.svg";
 import markerRed from "../assets/marker-red.svg";
 import markerYellow from "../assets/marker-yellow.svg";
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import BoardCell from "./BoardCell";
 import { CellValue, useGameDispatch, useGameState } from "../shared/GameContext";
 
@@ -15,12 +15,34 @@ type BoardProps = {
 const VirtualColumns = () => {
     const dispatch = useGameDispatch();
     const {currentPlayer} = useGameState();
+    const columnsRef = useRef(null);
 
     const columns = [0, 1, 2, 3, 4, 5, 6];
     const [selectedColumn, setSelectedColumn] = useState(0);
 
-    function dropCoin() {
-        dispatch({type: "DROP", columnIndex: selectedColumn});
+    useEffect(() => {
+        console.log(selectedColumn);
+
+        function keyDownHandler({key}: KeyboardEvent) {
+            if (key === "ArrowRight") {
+                setSelectedColumn((curr) => (curr === 6) ? 0 : curr + 1);
+            }
+            if (key === "ArrowLeft") {
+                setSelectedColumn((curr) => (curr === 0) ? 6 : curr - 1);
+            }
+            if (key === "Enter") {
+                dropCoin(selectedColumn);
+            }
+        }
+
+        window.addEventListener("keydown", keyDownHandler);
+        return () => {
+            window.removeEventListener("keydown", keyDownHandler);
+        };
+    }, [selectedColumn]);
+
+    function dropCoin(index: number) {
+        dispatch({type: "DROP", columnIndex: index});
     }
 
     function selectColumn(index: number) {
@@ -35,7 +57,12 @@ const VirtualColumns = () => {
 
     return <div id='virtual-columns'>
         {columns.map((i) =>
-            (<div key={`column-${i}`} className='virtual-column' onClick={dropCoin} onMouseOver={() => selectColumn(i)}>
+            (<div
+                key={`column-${i}`}
+                className='virtual-column'
+                onClick={() => dropCoin(selectedColumn)}
+                onMouseOver={() => selectColumn(i)}
+            >
                 {(selectedColumn === i) && renderMarker()}
             </div>)
         )}
